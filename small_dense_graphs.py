7c92a3c58ea111c339a108d678da7a89eec5ec06
BryannYeap <e0543723@u.nus.edu>
2023-04-04 02:19:13 +0800

    add small and medium test cases

--- small_dense_graphs.py
+++ small_dense_graphs.py
@@ -1,14 +1,38 @@
+import random
+import copy
+
 small_dense_graphs = [
     {
-        0: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        1: [0, 2, 3, 4, 5, 6, 7, 8, 9],
-        2: [0, 1, 3, 4, 5, 6, 7, 8, 9],
-        3: [0, 1, 2, 4, 5, 6, 7, 8, 9],
-        4: [0, 1, 2, 3, 5, 6, 7, 8, 9],
-        5: [0, 1, 2, 3, 4, 6, 7, 8, 9],
-        6: [0, 1, 2, 3, 4, 5, 7, 8, 9],
-        7: [0, 1, 2, 3, 4, 5, 6, 8, 9],
-        8: [0, 1, 2, 3, 4, 5, 6, 7, 9],
-        9: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        0: [1, 2, 3, 4],
+        1: [0, 2, 3, 4],
+        2: [0, 1, 3, 4],
+        3: [0, 1, 2, 4],
+        4: [0, 1, 2, 3]
     }
 ]
+
+for _ in range(9):
+    num_of_edges_to_remove = random.randint(1, 2)
+    new_dense_graph = copy.deepcopy(small_dense_graphs[0])
+    
+    for i in range(num_of_edges_to_remove):
+        while True:
+            from_node = random.randint(0, 10)
+            to_node = random.randint(0, 10)
+
+            while True:
+                if to_node == from_node or not (from_node in new_dense_graph and to_node in new_dense_graph[from_node]):
+                    from_node = random.randint(0, 10)
+                    to_node = random.randint(0, 10)
+                else:
+                    break
+
+            new_dense_graph[from_node].remove(to_node)
+            new_dense_graph[to_node].remove(from_node)
+
+            if new_dense_graph not in small_dense_graphs:
+                break
+
+            new_dense_graph = copy.deepcopy(small_dense_graphs[0])
+
+    small_dense_graphs.append(new_dense_graph)
--- test_local_search.py
+++ test_local_search.py
@@ -1,17 +1,42 @@
+import time
 from local_search import minimum_degree_spanning_tree
 from small_sparse_graphs import small_sparse_graphs
 from small_dense_graphs import small_dense_graphs
+from medium_sparse_graphs import medium_sparse_graphs
+from medium_dense_graphs import medium_dense_graphs
+# from large_sparse_graphs import large_sparse_graphs
+# from large_dense_graphs import large_dense_graph
 
 print("==========================================================================\n")
 
 for graph in range(len(small_sparse_graphs)):
+    t0 = time.time()
     print("TESTING SMALL SPARSE GRAPH", graph, ":")
-    print(minimum_degree_spanning_tree(small_sparse_graphs[graph]), "\n")
+    print(minimum_degree_spanning_tree(small_sparse_graphs[graph]))
+    print("Time Taken:", time.time() - t0, "\n")
 
 print("==========================================================================\n")
 
 for graph in range(len(small_dense_graphs)):
+    t0 = time.time()
     print("TESTING SMALL DENSE GRAPH", graph, ":")
-    print(minimum_degree_spanning_tree(small_dense_graphs[graph]), "\n")
+    print(minimum_degree_spanning_tree(small_dense_graphs[graph]))
+    print("Time Taken:", time.time() - t0, "\n")
+
+print("==========================================================================\n")
+
+for graph in range(len(medium_sparse_graphs)):
+    t0 = time.time()
+    print("TESTING MEDIUM SPARSE GRAPH", graph, ":")
+    print(minimum_degree_spanning_tree(medium_sparse_graphs[graph]))
+    print("Time Taken:", time.time() - t0, "\n")
+
+print("==========================================================================\n")
+
+for graph in range(len(medium_dense_graphs)):
+    t0 = time.time()
+    print("TESTING MEDIUM DENSE GRAPH", graph, ":")
+    print(minimum_degree_spanning_tree(medium_dense_graphs[graph]))
+    print("Time Taken:", time.time() - t0, "\n")
 
 print("==========================================================================\n")
