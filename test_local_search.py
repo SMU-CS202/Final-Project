9f4bd808d2341be277948f8d0011f47531d073dd
BryannYeap <e0543723@u.nus.edu>
2023-04-03 23:11:15 +0800

    add small sparse graph test cases

--- test_local_search.py
+++ test_local_search.py
@@ -1,6 +1,17 @@
 from local_search import minimum_degree_spanning_tree
 from small_sparse_graphs import small_sparse_graphs
+from small_dense_graphs import small_dense_graphs
+
+print("==========================================================================\n")
 
 for graph in range(len(small_sparse_graphs)):
     print("TESTING SMALL SPARSE GRAPH", graph, ":")
     print(minimum_degree_spanning_tree(small_sparse_graphs[graph]), "\n")
+
+print("==========================================================================\n")
+
+for graph in range(len(small_dense_graphs)):
+    print("TESTING SMALL DENSE GRAPH", graph, ":")
+    print(minimum_degree_spanning_tree(small_dense_graphs[graph]), "\n")
+
+print("==========================================================================\n")
