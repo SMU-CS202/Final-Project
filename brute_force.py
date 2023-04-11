5477323721fd2b93c935481c8214618f681c02c8
Nicole <vuwet467@gmail.com>
2023-04-11 23:39:17 +0800

    Refactored brute force

--- brute_force.py
+++ brute_force.py
@@ -1,15 +1,40 @@
 from itertools import chain, combinations
 
-def powerset(iterable):
-    s = list(iterable)
-    return list(chain.from_iterable(combinations(s, r) for r in range(len(s)+1)))
+# *********************************************************************************
+# *********************      MAIN FUNCTIONS       *********************************
+# *********************************************************************************
+def minimum_degree_spanning_tree(graph):
+    vertices = graph.keys() # get all vertices in the graph
+    all_valid_subgraphs = generate_subgraphs(graph) # get all valid subgraphs in the graph
+    spanning_trees = get_all_spanning_trees(all_valid_subgraphs, graph) # get all subgraphs which are spanning trees
+    mdst = get_smallest_degree(spanning_trees) # get the MDST among all the spanning trees
+    return mdst
 
-def get_degree(graph):
-    deg = -float('inf')
-    for v in graph:
-        deg = max(deg, len(v))
-    return deg
+# Get all valid subgraphs of the graph
+def generate_subgraphs(adj_list):
+    powerV = powerset(adj_list.keys()) # generate all possible vertice permutations
+    powerE = powerset(get_edges(adj_list)) # generate all possible edge permutations
+    subgraphs = [] # keep a list of all valid subgraphs IE the 
+    for V in powerV: # for every possible combination of vertices
+        for E in powerE: # for every possible combination of edges
+            accept = True # by default accept the combination
+            for edge in E:
+                # But if the edge vertices aren't present in the vertice set, it's not valid
+                if(checkIfEdgeVerticesInSet(edge, V) == False): 
+                    accept = False
+            if (accept):
+                subgraphs.append([V, E]) # dd valid subgraphs to the list of valid results
+    return subgraphs
 
+# Get all valid spanning trees, from the valid subgraphs
+def get_all_spanning_trees(subgraphs, adj_list):
+    spanning_trees = [] # store all valid spanning trees
+    for subgraph in subgraphs:
+        if (check_if_spanning(subgraph, adj_list)): # store it if it's a valid spanning tree
+            spanning_trees.append(subgraph)
+    return spanning_trees
+
+# Out of all the spanning trees, get the one with the smallest degree
 def get_smallest_degree(subgraphs):
     min_degree = float('inf')
     mdst = ""
@@ -19,7 +44,20 @@ def get_smallest_degree(subgraphs):
             min_degree = get_degree(subgraph)
     return mdst
 
-def get_edges(adj_list):
+# *********************************************************************************
+# *********************      HELPER FUNCTIONS       *******************************
+# *********************************************************************************
+def powerset(iterable): # get all possible combinations of elements in a set
+    s = list(iterable)
+    return list(chain.from_iterable(combinations(s, r) for r in range(len(s)+1)))
+
+def get_degree(graph): # get the degree of the graph, only for an adjacency list
+    deg = -float('inf')
+    for v in graph:
+        deg = max(deg, len(v))
+    return deg
+
+def get_edges(adj_list): # get all edges of the graph, only for an adjacency list 
     edges = []
     for key in adj_list.keys():
         for v in adj_list.get(key):
@@ -27,93 +65,84 @@ def get_edges(adj_list):
                 edges.append([key, v])
     return edges
 
-def checkIfVertexInSet(vertex, vertice_set):
+def checkIfVertexInSet(vertex, vertice_set): # check if a vertex can be found in a set of vertices
     found = False
     for v in vertice_set:
         if (vertex == v):
             found = True
     return found
 
-def checkIfEdgeVerticesInSet(edge, Vset):
+def checkIfEdgeVerticesInSet(edge, Vset): # check if both vertices of an edge are present in a set of vertices
     u_found = checkIfVertexInSet(edge[0], Vset)
     v_found = checkIfVertexInSet(edge[1], Vset)
     if (v_found and u_found):
         return True
     return False
 
-def convert_edgelist_to_adj(edges, num_nodes):
-    adj_matrix = [ [0] * num_nodes for i in range(num_nodes)]
+# Check if a vertex is contained within an edge
+def check_if_vertex_in_edge(vertex, edge):
+    if (vertex == edge[0] or vertex == edge[1]):
+        return True
+    return False
+
+def convert_edgelist_to_adj(edges, num_nodes): # convert an edgelist to adjacency list for manipulation
+    adj_matrix = [ [0] * num_nodes for i in range(num_nodes)] # first create an adjacency matrix
     for edge in edges:
         i = edge[0]
         j = edge[1]
         adj_matrix[i][j] = 1
         adj_matrix[j][i] = 1
+    
+    # Now build the adjacency list from the adjacency matrix
     adj_list = {}
-    # Now build the adjacency list
-    # First add keys
+
+    # First add keys, representing each node in the graph
     for i in range(num_nodes):
         adj_list[i] = []
     
-    # Add neighbours for each node
+    # Add neighbours for each  in the graph
     for i in range(num_nodes):
         for j in range(num_nodes):
             if (adj_matrix[i][j] == 1):
                 adj_list[i].append(j)
     return adj_list
 
+# For performing a DFS
 def DFS(source, adj_list, visited_arr):
-        # Mark the vertex visited
+        # The source vertex has already been visited, mark it as visited
         visited_arr[source] = True
 
-        # Visit the the neighbors
+        # Visit the the neighbors of the source vertex
         for i in range(len(adj_list[source])):
             neighbour = adj_list[source][i]
             if(visited_arr[neighbour] == False):
                 # Make recursive call from neighbor
                 DFS(neighbour, adj_list, visited_arr)
 
+# Perform a DFS on the graph. If all vertices have been visited, the graph is connected.
 def check_if_connected(adj_list):
+        # Store the number of vertices in the adjacency list
         num_vertices = len(adj_list.keys())
-
-        # Create array to store visited nodes
+        
+        # Create an array to store vertices which have been visited. Initialise to False by default
         visited = [False] * num_vertices
 
-        # Begin the DFS from vertex 0
+        # Start the DFS from the first vertex
         DFS(0, adj_list, visited)
 
-        # Check if all the vertices are visited, if yes then graph is connected
+        # The graph is connected if all nodes can be visited via a DFS
         count = 0
         for i in range(len(visited)):
             if(visited[i]):
                 count += 1
         if(num_vertices == count):
-            print("Given graph is connected")
-            print(adj_list)
             return True
         else:
-            print("Given graph is not connected")
             return False
 
-def generate_subgraphs(adj_list):
-    powerV = powerset(adj_list.keys()) # generate all possible vertice permutations
-    powerE = powerset(get_edges(adj_list)) # generate all possible edge permutations
-    subgraphs = []
-    for V in powerV:
-        for E in powerE:
-            accept = True
-            for edge in E:
-                if(checkIfEdgeVerticesInSet(edge, V) == False):
-                    accept = False
-            if (accept):
-                subgraphs.append([V, E])
-    return subgraphs
-
-def check_if_vertex_in_edge(vertex, edge):
-    if (vertex == edge[0] or vertex == edge[1]):
-        return True
-    return False
-
+# Check if a graph is spanning
 def check_if_spanning(subgraph, adj_list):
+    # A subgraph is spanning if it contains all vertices of the original graph and is connected
     subgraph_edges = subgraph[1]
     vertices = adj_list.keys() # get all vertices in the graph
     record_vertex_present = [False] * len(vertices) # keep track of the presence of each vertex in the graph
@@ -129,17 +158,3 @@ def check_if_spanning(subgraph, adj_list):
     if (check_if_connected(subgraph_to_adj)):
         return True
     return False
-
-def get_all_spanning_trees(subgraphs, adj_list):
-    spanning_trees = []
-    for subgraph in subgraphs:
-        if (check_if_spanning(subgraph, adj_list)):
-            spanning_trees.append(subgraph)
-    return spanning_trees
-
-def minimum_degree_spanning_tree(graph):
-    vertices = graph.keys() # get all verticees
-    all_valid_subgraphs = generate_subgraphs(graph)
-    spanning_trees = get_all_spanning_trees(all_valid_subgraphs, graph)
-    mdst = get_smallest_degree(spanning_trees)
-    return mdst
